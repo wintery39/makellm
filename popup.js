2d6e9d94e4986246be4262d2684367c7fab62a93
wintery39 <wintery39@naver.com>
2024-09-22 14:54:56 +0900

    success modify, but it's not pretty

--- popup.js
+++ popup.js
@@ -20,3 +20,33 @@ document.addEventListener('DOMContentLoaded', () => {
     return document.documentElement.outerHTML;
   }
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+  const changeHtmlButton = document.getElementById('change-html');
+  
+  // change-html 버튼이 존재하는지 확인
+  if (changeHtmlButton) {
+    changeHtmlButton.addEventListener('click', () => {
+      chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        chrome.scripting.executeScript({
+          target: { tabId: tabs[0].id },
+          function: modifyHtmlAndCss
+        });
+      });
+    });
+  } else {
+    console.error("change-html 버튼을 찾을 수 없습니다.");
+  }
+});
+
+function modifyHtmlAndCss() {
+  const specificTkBoxDiv = document.querySelector('div.tk_box > fieldset.box01');
+  if (specificTkBoxDiv) {
+    const parentDiv = specificTkBoxDiv.closest('.tk_box');
+    if (parentDiv) {
+      parentDiv.classList.remove('tk_box'); 
+      parentDiv.classList.add('mynewcss');
+      console.log('tk_box div found:', parentDiv);
+    }
+  }
+}
